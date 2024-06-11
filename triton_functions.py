e18b46b27285806df233758f35026108b05d729d
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-06-11 20:29:13 +0100

    fix benchmark

--- triton_functions.py
+++ triton_functions.py
@@ -151,7 +151,7 @@ def int8_matmul_block64_rowwise_dequantize(a, b, state_x, state_w, bias=None):
     if b.stride(0) > 1 and b.stride(1) > 1:
         b = b.contiguous()
     # checks constraints
-    print(a.shape, b.shape)
+    # print(a.shape, b.shape)
     assert a.shape[1] == b.shape[0], "incompatible dimensions"
     M, K = a.shape
     _, N = b.shape
@@ -327,7 +327,7 @@ def copy_then_matmul(a, b):
 @triton.testing.perf_report(
     triton.testing.Benchmark(
         x_names=['M', 'N', 'K'],  # Argument names to use as an x-axis for the plot
-        x_vals=[256 * i for i in range(2, 16, 2)],  # Different possible values for `x_name`
+        x_vals=[1024 * i for i in range(2, 16, 2)],  # Different possible values for `x_name`
         line_arg='provider',  # Argument name whose value corresponds to a different line in the plot
         # Possible values for `line_arg`
         line_vals=['cublas', 'blockwise_int8', 'blockwise_int8_quantise', 'cublas+copy'],
@@ -340,13 +340,15 @@ def copy_then_matmul(a, b):
         args={},
     ))
 def benchmark(M, N, K, provider):
-    print("here")
+    K = 512
     a = torch.randn((M, K), device='cuda', dtype=INPUT_OUTPUT_TORCH_TYPE)
     b = torch.randn((K, N), device='cuda', dtype=INPUT_OUTPUT_TORCH_TYPE)
     quantiles = [0.5, 0.2, 0.8]
     if provider == 'cublas+copy':
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: copy_then_matmul(a, b), quantiles=quantiles)
     if provider == 'cublas':
+        a = a.to(torch.bfloat16)
+        b = b.to(torch.bfloat16)
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch.matmul(a, b), quantiles=quantiles)
     if provider == 'blockwise_int8':
         b = b.t().contiguous()
@@ -361,31 +363,31 @@ def benchmark(M, N, K, provider):
         b = b.t().contiguous()
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: bench_target(a, b), quantiles=quantiles) 
     perf = lambda ms: 2 * M * N * K * 1e-12 / (ms * 1e-3)
-    # return perf(ms), perf(max_ms), perf(min_ms)
-    return ms, max_ms, min_ms
+    return perf(ms), perf(max_ms), perf(min_ms)
+    # return ms, max_ms, min_ms
 
 
 if __name__ == "__main__":
-    def block_matmul_is_close(w = 253, h = 257, k = 234, factor=1):
-        a = torch.randn((w, k), device="cuda", dtype=INPUT_OUTPUT_TORCH_TYPE) * factor
-        b = torch.randn((h, k), device="cuda", dtype=INPUT_OUTPUT_TORCH_TYPE) / factor
-        a_int8, a_state = quantize_block_rowwise(a)
-        b_int8, b_state = quantize_block_rowwise(b)
-        c_hat = int8_matmul_block64_rowwise_dequantize(a_int8, b_int8.t(), a_state, b_state)
-        c = torch.matmul(a, b.t())
-        print(c)
-        print(c_hat)
-        print(torch.max(c - c_hat))
+    # def block_matmul_is_close(w = 253, h = 257, k = 234, factor=1):
+    #     a = torch.randn((w, k), device="cuda", dtype=INPUT_OUTPUT_TORCH_TYPE) * factor
+    #     b = torch.randn((h, k), device="cuda", dtype=INPUT_OUTPUT_TORCH_TYPE) / factor
+    #     a_int8, a_state = quantize_block_rowwise(a)
+    #     b_int8, b_state = quantize_block_rowwise(b)
+    #     c_hat = int8_matmul_block64_rowwise_dequantize(a_int8, b_int8.t(), a_state, b_state)
+    #     c = torch.matmul(a, b.t())
+    #     print(c)
+    #     print(c_hat)
+    #     print(torch.max(c - c_hat))
 
     # block_matmul_is_close()
     # block_matmul_is_close(129, 266, 75)
     # block_matmul_is_close(389, 138, 45)
     # block_matmul_is_close(389, 138, 45, 5)
     # block_matmul_is_close(389, 138, 45, 10)
-    block_matmul_is_close(256, 512,512)
-    block_matmul_is_close(1024, 512,512)
+    # block_matmul_is_close(256, 512,512)
+    # block_matmul_is_close(1024, 512,512)
 
-    # benchmark.run(show_plots=True, print_data=True)
+    benchmark.run(show_plots=True, print_data=True)
     # quantise_benchmark.run(show_plots=True, print_data=True)
     # benchmark.run(show_plots=True, print_data=True)
     # benchmark.run(show_plots=True, print_data=True)
